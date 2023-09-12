9d41dae0c550e6512ff1120c71b0e2732998bbc9
Daniel Hart <super.awesome.hartd@gmail.com>
2023-09-11 23:01:20 -0400

    Removed Comment

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,3 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
-
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
 -- In your plugin files, you can:
